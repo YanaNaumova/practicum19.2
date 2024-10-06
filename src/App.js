caa5b5404866683e3d5fd2b7472fcb4dbcc5c338
YanaNaumova <naumovayana87@gmail.com>
2024-10-06 20:03:04 +0200

    practicum rsuite

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { List, Input, InputGroup, Panel } from "rsuite";
+import PlusIcon from "@rsuite/icons/Plus";
 
 function App() {
+  const [element, setElement] = useState("");
+  const [elementsList, setElementsList] = useState([]);
+
+  function addElement() {
+    setElementsList([...elementsList, element]);
+    setElement("");
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div style={{ padding: "20px" }}>
+        <InputGroup
+          inside
+          style={{
+            display: "flex",
+            alignItems: "center",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Input
+            value={element}
+            onChange={setElement}
+            placeholder="Введите новый элемент"
+            style={{
+              padding: "3px",
+              maxWidth: "300px",
+              width: "100%",
+            }}
+          />
+          <InputGroup.Button
+            style={{
+              backgroundColor: "#1675e0",
+              border: "none",
+              padding: "5px 10px",
+              height: "100%",
+            }}
+          >
+            <PlusIcon
+              style={{ fontSize: "16px", color: "#fff" }}
+              onClick={addElement}
+            />
+          </InputGroup.Button>
+        </InputGroup>
+      </div>
+
+      <Panel bordered>
+        <List size="sm">
+          {elementsList.map((item, index) => (
+            <List.Item
+              key={index}
+              index={index}
+              style={{
+                border: "1px solid black",
+                borderRadius: "10px",
+                padding: "10px",
+                margin: "5px",
+                maxWidth: "600px",
+              }}
+            >
+              {item}
+            </List.Item>
+          ))}
+        </List>
+      </Panel>
+    </>
   );
 }
 
